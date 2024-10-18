5f98c277269570aa26d31f057792853a7abd5cb6
SonBuik3 <sonbui090703@gmail.com>
2024-10-18 20:52:02 +0700

    create-map

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,27 +2,20 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    public GameObject[] levelPrefabs;
-    public int currentLevel;
+    public GameObject[] levels;
 
     void Start()
     {
-        LoadLevel(currentLevel);
-    }
-
-    void LoadLevel(int levelIndex)
-    {
-
-        if (levelIndex >= 0 && levelIndex < levelPrefabs.Length)
+        if (GameGlobal.level < levels.Length)
         {
             Vector3 position = new Vector3(0, 0, 0);
             Quaternion rotation = Quaternion.identity;
 
-            Instantiate(levelPrefabs[levelIndex], position, rotation);
+            Instantiate(levels[GameGlobal.level], position, rotation);
         }
         else
         {
-            Debug.LogError("Invalid level index: " + levelIndex);
+            Debug.LogError("Invalid level index: " + levels);
         }
     }
 }
